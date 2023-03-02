198974a4e1d386ac6b3861b382668ab866e6acf0
Vasisualey <basilnekrasov@mail.ru>
2023-03-02 18:37:38 +0300

    сделал 67 задачу

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,1 +1,29 @@
 ﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+Console.Clear();
+int n = GetNumberFromUser($"Введите число ", "Ошибка ввода!");
+int result = GetResult(n);
+Console.Write($"{n} -> {result}");
+
+
+int GetNumberFromUser(string message, string errorMessage) // это проверка правильности метода
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+
+
+int GetResult(int n)
+{
+    if (n == 0) return 0;
+    else
+    {
+        return n%10 + GetResult(n/10);
+    }
+}
